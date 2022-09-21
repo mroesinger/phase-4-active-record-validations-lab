4007f0219b3a1fb784e442be9b605cb925920852
michaelroesinger <mroesinger@gmail.com>
2022-09-21 15:23:39 -0700

    commit tree eed84da7430beb2ee917f3ffb884d43645067cc6

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]   }
+
+    validate :title
+      def must_be_clickbait
+          if title.match?("Won't Believe""Secret""Top [number]""Guess")
+            errors.add(:title, "this is an errror")
+          else
+    end
 end
